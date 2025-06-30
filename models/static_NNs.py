b67946495ebe923f3814582899693a8598799521
Clara Galimberti <claraluciagalimberti@gmail.com>
2025-06-30 19:31:05 +0000

    comment added

--- models/static_NNs.py
+++ models/static_NNs.py
@@ -5,7 +5,7 @@ from torch import nn
 class FCNN(nn.Module):
     def __init__(self, dim_in, dim_out, dim_hidden, act=nn.Tanh):
         super(FCNN, self).__init__()
-
+        print("!!!!! We are using the FCNN of the static_NN file!!!!!!!")
         self.network = nn.Sequential(
             nn.Linear(dim_in, dim_hidden, bias=False), act(),
             nn.Linear(dim_hidden, dim_hidden, bias=False), act(),
